909f8bc6827212e0dfd320daadaf4727a448376f
Zakaria Aboukhriss <zak@weddingplanner.co.uk>
2020-01-31 23:52:22 +0100

    Adding all assets files (Huge one)
    Signed-off-by: Zakaria Aboukhriss <zak@weddingplanner.co.uk>

---
+++ Assets/world/MainCameraScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraScript : MonoBehaviour
+{
+	private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	player = GameObject.Find("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if(player != null){
+	    	transform.position = new Vector3(
+	    			player.transform.position.x,
+	    			player.transform.position.y+50,
+	    			player.transform.position.z
+	    		); 
+    	} else if(player == null){
+    		Debug.Log("////// WARNING //////");
+    		Debug.Log("The player GameObject name was not set properly in MainCameraScript.");
+    		Debug.Log("////// WARNING //////");    	
+    	}
+    }
+}
