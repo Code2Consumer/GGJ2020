7376ba5d5266911322fa14fe6b3c2ccac99793b0
Zakaria Aboukhriss <zak@weddingplanner.co.uk>
2020-01-31 23:50:11 +0100

    wip
    Signed-off-by: Zakaria Aboukhriss <zak@weddingplanner.co.uk>

--- Assets/player/JetPackScript.cs
+++ Assets/player/JetPackScript.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class JetPackScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody rb;
+
+    private float time = 0.0f;
+    private bool isMoving = false;
+    private bool isJumpPressed = false;
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        isJumpPressed = Input.GetButtonDown("Jump");
+    }
+
+    void FixedUpdate()
+    {
+        if (isJumpPressed)
+        {
+            // the cube is going to move upwards in 10 units per second
+            rb.velocity = new Vector3(10, 0, 0);
+            isMoving = true;
+            Debug.Log("jump");
+        }
+
+        if (isMoving)
+        {
+            // when the cube has moved for 10 seconds, report its position
+            time = time + Time.fixedDeltaTime;
+            if (time > 10.0f)
+            {
+                Debug.Log(gameObject.transform.position.y + " : " + time);
+                time = 0.0f;
+            }
+        }
     }
 }
