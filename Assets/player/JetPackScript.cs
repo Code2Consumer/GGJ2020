befa02edace1ac442e818a18a9b9a0cb559eeeaa
Zakaria Aboukhriss <zak@weddingplanner.co.uk>
2020-02-01 09:59:29 +0100

    Upgrade mooves
    Signed-off-by: Zakaria Aboukhriss <zak@weddingplanner.co.uk>

--- Assets/player/JetPackScript.cs
+++ Assets/player/JetPackScript.cs
@@ -8,10 +8,12 @@ public class JetPackScript : MonoBehaviour
     private Vector3 rotation;
     private float h;
     private float v;	
-    private float speed = 3;
+    private float mainSpeed = 6;
+    private float speed = 6;
 
     void Start()
     {
+    	
     }
 
     void Update()
@@ -19,9 +21,23 @@ public class JetPackScript : MonoBehaviour
         h = Input.GetAxisRaw("Horizontal");
 		v = Input.GetAxisRaw("Vertical");
 
+    	speed = 0 ;
+    	speed = mainSpeed ;
+
 		if(h!=0 || v != 0){
+			if(h!=0 && v != 0){
+				speed = mainSpeed * 0.75f;
+			} else {
+				speed = mainSpeed ;
+			} 
+		}
+
+		if(speed != 0){
+
 			direction = (new Vector3(h*speed, 0, v*speed) * Time.deltaTime * speed ) + transform.position;
 		   	transform.position = Vector3.MoveTowards(transform.position, direction, speed );
+		    //Vector3 position = Vector3.Lerp(transform.position, new Vector3(direction.x*2, transform.position.y, direction.z*2), Time.deltaTime * 0.5f);
+
 
 			rotation = new Vector3(
 					h + transform.position.x,
--- Assets/world/MainCameraScript.cs
+++ Assets/world/MainCameraScript.cs
@@ -5,22 +5,30 @@ using UnityEngine;
 public class MainCameraScript : MonoBehaviour
 {
 	private GameObject player;
+    public float damping = 1;
+    Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
     	player = GameObject.Find("player");
+        offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
     	if(player != null){
-	    	transform.position = new Vector3(
-	    			player.transform.position.x,
-	    			player.transform.position.y+5,
-	    			player.transform.position.z
-	    		); 
+	    	 transform.position = new Vector3(
+	    	 		player.transform.position.x,
+	    	 		player.transform.position.y+20,
+	    	 		player.transform.position.z
+	    	 	); 
+		   // Vector3 desiredPosition = player.transform.position + offset;
+	       // Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
+	       // transform.position = position;
+	 //
+	       // transform.LookAt(player.transform.position);
     	} else if(player == null){
     		Debug.Log("////// WARNING //////");
     		Debug.Log("The player GameObject name was not set properly in MainCameraScript.");
