f58216bede03cd70c932379a785bd9b03a9cce29
Zakaria Aboukhriss <zak@weddingplanner.co.uk>
2020-02-01 02:54:57 +0100

    Adding player moovements
    Signed-off-by: Zakaria Aboukhriss <zak@weddingplanner.co.uk>

--- Assets/player/JetPackScript.cs
+++ Assets/player/JetPackScript.cs
@@ -3,43 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class JetPackScript : MonoBehaviour
-{
-    public Rigidbody rb;
-
-    private float time = 0.0f;
-    private bool isMoving = false;
-    private bool isJumpPressed = false;
-
+{  
+    private Vector3 direction;
+    private Vector3 rotation;
+    private float h;
+    private float v;	
+    private float speed = 3;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        isJumpPressed = Input.GetButtonDown("Jump");
+        h = Input.GetAxisRaw("Horizontal");
+		v = Input.GetAxisRaw("Vertical");
+
+		if(h!=0 || v != 0){
+			direction = (new Vector3(h*speed, 0, v*speed) * Time.deltaTime * speed ) + transform.position;
+		   	transform.position = Vector3.MoveTowards(transform.position, direction, speed );
+
+			rotation = new Vector3(
+					h + transform.position.x,
+					transform.position.y,
+					v + transform.position.z
+				);
+
+			transform.LookAt(rotation);
+		}
+	 
+
     }
 
     void FixedUpdate()
     {
-        if (isJumpPressed)
-        {
-            // the cube is going to move upwards in 10 units per second
-            rb.velocity = new Vector3(10, 0, 0);
-            isMoving = true;
-            Debug.Log("jump");
-        }
 
-        if (isMoving)
-        {
-            // when the cube has moved for 10 seconds, report its position
-            time = time + Time.fixedDeltaTime;
-            if (time > 10.0f)
-            {
-                Debug.Log(gameObject.transform.position.y + " : " + time);
-                time = 0.0f;
-            }
-        }
+        // if (isMoving)
+        // {
+        //     // when the cube has moved for 10 seconds, report its position
+        //     time = time + Time.fixedDeltaTime;
+        //     if (time > 10.0f)
+        //     {
+        //         Debug.Log(gameObject.transform.position.y + " : " + time);
+        //         time = 0.0f;
+        //     }
+        // }
     }
 }
